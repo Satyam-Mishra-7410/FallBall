a8aa96cb78b951c921a4fb910b58be389bbeeeab
DESKTOP-K290BGS\mishr <mishrasatyam28@gmail.com>
2018-06-17 15:30:31 +0530

    Added the project folder

---
+++ Project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float _RotationSpeed = 100.0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0.0f, 0.0f, _RotationSpeed * Time.deltaTime);		
+	}
+}
